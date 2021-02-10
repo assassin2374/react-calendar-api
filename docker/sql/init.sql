5b00e2257b112e9c5e0918aaf129af1bb7334246
assassin_box <assassin543@outlook.jp>
2021-02-10 23:07:15 +0900

    year,month,dayをdateに一括で管理するように変更

--- docker/sql/init.sql
+++ docker/sql/init.sql
@@ -1,19 +1,17 @@
 CREATE TABLE schedule (
   id int NOT NULL AUTO_INCREMENT,
   user_id int NOT NULL,
-  year int NOT NULL,
-  month tinyint NOT NULL,
-  day tinyint NOT NULL,
+  date date NOT NULL,
   contents varchar(255) NOT NULL DEFAULT '',
   created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Schedule';
 
-INSERT INTO schedule (user_id, year, month, day, contents) VALUES
-(1, 2021, 1, 21, '仕事'),
-(1, 2021, 1, 22, '仕事2'),
-(1, 2021, 1, 23, '仕事3錬金');
+INSERT INTO schedule (user_id, date, contents) VALUES
+(1, '2021-1-21', '仕事'),
+(1, '2021-1-22', '仕事2'),
+(1, '2021-1-23', '仕事3錬金');
 
 
 -- 予定オブジェクト

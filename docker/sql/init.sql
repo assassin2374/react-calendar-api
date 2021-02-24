6b1e820487069d92e28d4b2e49dfcb247a33fbca
assassin_box <assassin543@outlook.jp>
2021-02-25 00:32:47 +0900

    ymd_dateに変更

--- docker/sql/init.sql
+++ docker/sql/init.sql
@@ -1,14 +1,14 @@
 CREATE TABLE schedule (
   id int NOT NULL AUTO_INCREMENT,
   user_id int NOT NULL,
-  date date NOT NULL,
+  ymd_date date NOT NULL,
   contents varchar(255) NOT NULL DEFAULT '',
   created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Schedule';
 
-INSERT INTO schedule (user_id, date, contents) VALUES
+INSERT INTO schedule (user_id, ymd_date, contents) VALUES
 (1, '2021-1-21', '仕事'),
 (1, '2021-1-22', '仕事2'),
 (1, '2021-1-23', '仕事3錬金');
